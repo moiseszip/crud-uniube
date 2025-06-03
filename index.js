8ce0cb48a54410f52bd058dd9589dd4e01dbf5a3
moiseszip <moisesasdm226@gmail.com>
2025-06-03 20:29:27 -0300

    conectando ao mongodb

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 7000;
+const MONGOURL = process.env.MONGO_URL || 'mongodb://localhost:27017/mern';
+
+mongoose
+    .connect(MONGOURL)
+    .then(() => {
+        console.log('Conectado ao MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}`);
+        });
+    })
+    .catch((error) => console.log(error));
