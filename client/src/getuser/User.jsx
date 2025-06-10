dd39b1b7314c0cc4fed042828f030da9ed7fbfdc
moiseszip <moisesasdm226@gmail.com>
2025-06-10 20:22:37 -0300

    exibição dos dados do DB na tabela

--- client/src/getuser/User.jsx
+++ client/src/getuser/User.jsx
@@ -1,44 +1,65 @@
-import React from 'react'
-import './User.css'
-
+import React, { useEffect, useState } from "react";
+import "./User.css";
+import axios from "axios";
 
 const User = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/api/users");
+        setUsers(response.data);
+      } catch (error) {
+        console.log("Erro", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className='userTable'>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Adicionar Usuário <i class="fa-solid fa-user-plus"></i>
-        </button>
+    <div className="userTable">
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Adicionar Usuário <i class="fa-solid fa-user-plus"></i>
+      </button>
 
-        <table className='table table-bordered'>
-            <thead>
-                <tr>
-                    <th scope='col'>S.No.</th>
-                    <th scope='col'>Nome</th>
-                    <th scope='col'>Email</th>
-                    <th scope='col'>Endereço</th>
-                    <th scope='col'>Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>joao</td>
-                    <td>joao@email.com</td>
-                    <td>brasil</td>
-                    <td className='actionButtons'>
-                        <button type="button" class="btn btn-info">
-                            <i class="fa-solid fa-pen-to-square"></i>
-                        </button>
-                        <button type="button" class="btn btn-danger">
-                            <i class="fa-solid fa-trash"></i>
-                        </button>
-                    </td>
-                </tr>
-            </tbody>    
-        </table>
-
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">S.No.</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Email</th>
+            <th scope="col">Endereço</th>
+            <th scope="col">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => {
+            return (
+              <tr>
+                <td>{index+1}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.address}</td>
+                <td className="actionButtons">
+                  <button type="button" class="btn btn-info">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                  </button>
+                  <button type="button" class="btn btn-danger">
+                    <i class="fa-solid fa-trash"></i>
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
