96c341946be03cbccb62fa19e154081866658045
moiseszip <moisesasdm226@gmail.com>
2025-06-10 20:43:47 -0300

    criação form de add usuário

--- client/src/addUser/AddUser.jsx
+++ client/src/addUser/AddUser.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import "./AddUser.css";
+import { Link } from "react-router-dom";
 
 const AddUser = () => {
   return (
-    <div>AddUser Form</div>
-  )
-}
+    <div className="addUser">
+        <Link type="button" className="btn btn-secondary">
+          <i className="fa-solid fa-backward"></i> Voltar
+        </Link>
+      <h3>Adicionar Novo Usuário</h3>
+      <form className="addUserForm">
+        <div className="inputGroup">
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            autoComplete="off"
+            placeholder="Fulano da Silva"
+          />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            autoComplete="off"
+            placeholder="fulano@email.com"
+          />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="address">Endereço:</label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            autoComplete="off"
+            placeholder="Rua Brasil, 123, Bairro Centro"
+          />
+        </div>
+        <div className="inputGroup">
+          <button type="submit" className="btn btn-primary">
+            Salvar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default AddUser
+export default AddUser;
